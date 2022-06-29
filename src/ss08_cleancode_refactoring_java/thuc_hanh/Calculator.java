a1766f78a66683cf8a3d8f073bde3875e18518a7
A0322I1huynhvannam <huynhvannam.tk1@gmail.com>
2022-06-29 18:01:05 +0700

    up

---
+++ src/ss08_cleancode_refactoring_java/thuc_hanh/Calculator.java
@@ -0,0 +1,22 @@
+package ss08_cleancode_refactoring_java.thuc_hanh;
+
+public class Calculator {
+
+    public static int calculate(int a, int b, char o) {
+        switch (o) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b != 0)
+                    return a / b;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}

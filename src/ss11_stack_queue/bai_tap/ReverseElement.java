f0effba2fd5404f66ffd42b4954e6d5ae8c8a709
A0322I1huynhvannam <huynhvannam.tk1@gmail.com>
2022-09-07 17:50:29 +0700

    update

--- src/ss11_stack_queue/bai_tap/ReverseElement.java
+++ src/ss11_stack_queue/bai_tap/ReverseElement.java
@@ -6,14 +6,13 @@ public class ReverseElement {
     public static void main(String[] args) {
         Stack<Integer> Stack = new Stack();
 
-        Stack.push(1);
+        Stack.push(6);
         Stack.push(2);
         Stack.push(3);
         Stack.push(4);
-        Stack.push(5);
-        int temp = Stack.size();
+        Stack.push(7);
         Stack<Integer> wStack = new Stack<>();
-        for (int i = 0 ; i<temp;i++){
+        for (int i = 0 ; i<Stack.size();i++){
             wStack.push(Stack.pop());
         }
         System.out.println(wStack);

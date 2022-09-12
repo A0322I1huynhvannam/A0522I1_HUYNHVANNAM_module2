d0df7845b7f8492f3aeaec283e000e5210591b30
A0322I1huynhvannam <huynhvannam.tk1@gmail.com>
2022-09-12 18:12:15 +0700

    update

--- src/ss13_tim_kiem/bai_tap/BinarySearch.java
+++ src/ss13_tim_kiem/bai_tap/BinarySearch.java
@@ -18,6 +18,6 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int [] myArray = {1,2,3,4,5,6,7,8,9,10};
-        System.out.println(Search(myArray,10));
+        System.out.println(Search(myArray,5));
     }
 }
